4c1c5073795d793d658dd80772200b35e831224c
Harithmech <harithmech@gmail.com>
2021-08-09 21:31:50 +0530

    learnt decorators

--- decorators.py
+++ decorators.py
@@ -5,19 +5,21 @@ def about(func):
     def wrapper(*args, **kwargs):
         func(*args, **kwargs)
         print("I am from Bangalore")
+        print("I study at ", args[0])
+        print("My phone number is: ", args[1])
         print("-------------------")
     return wrapper
 
 
 @about
-def hari():
+def hari(col, *args):
     print("Harith is my name")
 
 
 @about
-def harshi():
+def harshi(col, *args):
     print("Harshith is my name")
 
 
-hari()
-harshi()
+hari("Mvjce", 7835549)
+harshi("alvas", 123467)
