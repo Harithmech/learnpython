a8b0f2fa9fec1589bb462df4923641d16f62691c
Harithmech <harithmech@gmail.com>
2021-08-09 21:26:02 +0530

    added decorator funcation

--- decorators.py
+++ decorators.py
@@ -1,9 +1,20 @@
 # decorators
+# adding a function to store common details
 
+def about(func):
+    def wrapper(*args, **kwargs):
+        func(*args, **kwargs)
+        print("I am from Bangalore")
+        print("-------------------")
+    return wrapper
+
+
+@about
 def hari():
     print("Harith is my name")
 
 
+@about
 def harshi():
     print("Harshith is my name")
 
