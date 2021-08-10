ebf0e64a102038256ff16fdd5c9d5929c2e0e242
Harithmech <harithmech@gmail.com>
2021-08-10 10:04:47 +0530

    description

--- exception_h.py
+++ exception_h.py
@@ -1,3 +1,6 @@
+# whenever you fell that a part of program gives an error
+# put the block of code inside try
+# except will be the block of lines to output when the error occurs while runtime.
 try:
     file = open('name.txt')
 except Exception:
