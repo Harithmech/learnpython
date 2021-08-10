b905adcb750397dd2c6453ea70e4a5da3e9590ae
Harithmech <harithmech@gmail.com>
2021-08-10 10:14:24 +0530

    different types of exception handling

--- exception_h.py
+++ exception_h.py
@@ -1,7 +1,15 @@
 # whenever you fell that a part of program gives an error
 # put the block of code inside try
 # except will be the block of lines to output when the error occurs while runtime.
+# try block is executed line by line! only after first line satisfies flow goes to next line
+
+
+# order of precedence of except matters
 try:
-    file = open('name.txt')
+    file = open('trial.txt')
+    a = num
+except FileNotFoundError:  # exception can be in many form.
+    print("file not found!")  # we get custom error instead of long lines!
+
 except Exception:
-    print("file not found!")
+    print("wrong variable declaration")
