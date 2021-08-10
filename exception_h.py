f1919d380108ce7053e7ce4ed4c3330dfad4ed72
Harithmech <harithmech@gmail.com>
2021-08-10 10:29:40 +0530

    making use if else

--- exception_h.py
+++ exception_h.py
@@ -1,13 +1,17 @@
+# try
 # whenever you fell that a part of program gives an error
 # put the block of code inside try
 # except will be the block of lines to output when the error occurs while runtime.
 # try block is executed line by line! only after first line satisfies flow goes to next line
 
-
+# except
 # order of precedence of except matters
 # when you dont want custion msg use: except Exception as e
+
+# else
+# if try does not raise an exception control flow goes to else
 try:
-    file = open('tial.txt')
+    file = open('trial.txt')
     a = num
 except FileNotFoundError as e:  # exception can be in many form.
     print(e)  # we get custom error instead of long lines!
