97c358c34b1c4ddb1a585ad09a0b279b58c72d35
Harithmech <harithmech@gmail.com>
2021-08-10 10:42:36 +0530

    all about exception handling

--- exception_h.py
+++ exception_h.py
@@ -10,11 +10,33 @@
 
 # else
 # if try does not raise an exception control flow goes to else
+# we can use else content in try, but inorder to be specific about the error we dont
+
+# finally
+# it runs even if there is an exception
+
+# raise is usedd to manually create an exception
+from os import error
+
+
 try:
     file = open('trial.txt')
-    a = num
+    #a = num
+    if file.name == "trial.txt":
+        raise Exception
+
 except FileNotFoundError as e:  # exception can be in many form.
     print(e)  # we get custom error instead of long lines!
 
+except Exception:
+    print("error")
+
 except Exception as e:
     print(e)
+
+else:
+    print(file.read())
+    file.close()
+
+finally:
+    print("------Done---------")
