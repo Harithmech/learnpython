4aa412b3c2e7b53d832b29962f536264bc7b7cb2
Harithmech <harithmech@gmail.com>
2021-08-19 18:57:50 +0530

    array

---
+++ array/max3sum.py
@@ -0,0 +1,16 @@
+arr = [12, 5, 4, 23, 7, 100, 1]  # 0-6
+
+n = len(arr)  # 7
+
+maxi, maxl, maxr = 0, 0, 0
+
+for i in range(1, n-1):  # 1-6
+    maxl = 0
+    for j in range(0, i):
+        maxl = max(maxl, arr[j])
+    maxr = 0
+    for k in range(i+1, n):
+        maxr = max(maxr, arr[k])
+    maxi = max(maxi, (maxl + arr[i] + maxr))
+
+print(maxi)
