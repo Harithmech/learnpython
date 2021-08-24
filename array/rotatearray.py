6887071968482db732a1bdcf8e3e212571941f20
Harithmech <harithmech@gmail.com>
2021-08-24 21:09:18 +0530

    order n space and time complexity

--- array/rotatearray.py
+++ array/rotatearray.py
@@ -3,13 +3,33 @@
 # rotate the array by k units
 # clockwise --> pick an element from end and place it upfront
 
-arr = [1, 5, 2, 4, 3]
 
-k = 3
-for i in range(k):
+# time complexity n2
+# space complexity o(1)
+""" arr = [1, 5, 2, 4, 3]
+
+k = 6
+for i in range(k % len(arr)):
     temp = arr[-1]
     for j in range(len(arr)-1, 0, -1):
         arr[j] = arr[j-1]
     arr[0] = temp
 
-print(arr)
+print(arr) """
+
+
+# time complecity o(n)
+# space complexity o(n)
+
+arr = [1, 2, 3, 4, 5]
+
+arr1 = [0 for i in range(len(arr))]
+
+k = 3
+
+N = len(arr)
+for i in range(0, len(arr)):
+    # if(i % 3 == 0):
+    arr1[(i+k) % N] = arr[i]
+
+print(arr1)
